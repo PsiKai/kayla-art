edfd890baf91f4d6ac37e8146e706ed5fa032e6b
David Irvin <davidirvin47@gmail.com>
2024-02-04 14:42:41 -0700

    Create initial DB connection

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+import "dotenv/config"
 import express from "express"
 import apiRouter from "./routes/api/index.js"
 import appRouter from "./routes/app/index.js"
+import initDB from "./db/init.js"
 
 const app = express()
+await initDB()
 
 app.use("/api", apiRouter)
 app.use("/", appRouter)
