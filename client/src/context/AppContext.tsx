34886205655271eea2a907bd15797bee8b18e10f
David Irvin <davidirvin47@gmail.com>
2024-03-17 17:03:46 -0600

    Stand up initial context provider

---
+++ client/src/context/AppContext.tsx
@@ -0,0 +1,53 @@
+import React, { createContext, useReducer } from "react"
+
+type TArtWork = {
+  id: string
+  title: string | null
+  category: "Photography" | "Illustration"
+  thumbnail: string
+  collection: string
+  subcategory: string
+}
+
+type TAppState = {
+  user: string | null
+  art: TArtWork[]
+}
+
+type Props = {
+  children: React.ReactNode | React.ReactNode[]
+}
+
+type TProvider = {
+  state: TAppState
+  dispatch: React.Dispatch<{ type: string; payload: Partial<TAppState> }>
+}
+
+const initialAppState: TAppState = {
+  user: null,
+  art: [],
+}
+
+const initialAppContext: TProvider = { state: initialAppState, dispatch: () => {} }
+
+export const AppContext = createContext(initialAppContext)
+
+export const AppProvider: React.FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(appReducer, initialAppState)
+
+  return <AppContext.Provider value={{ state, dispatch }}>{children}</AppContext.Provider>
+}
+
+function appReducer(
+  state: TAppState,
+  action: { type: string; payload: Partial<TAppState> },
+): TAppState {
+  switch (action.type) {
+    case "SET_USER":
+      return { ...state, user: action.payload.user! }
+    case "SET_ART":
+      return { ...state, art: action.payload.art! }
+    default:
+      return state
+  }
+}
