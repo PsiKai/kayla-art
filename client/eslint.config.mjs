292f5e442375dac0b46efa68de02a10ff8d13a2c
David Irvin <davidirvin47@gmail.com>
2025-04-18 17:14:28 -0600

    Update client npm dependencies

---
+++ client/eslint.config.mjs
@@ -0,0 +1,78 @@
+import { defineConfig, globalIgnores } from "eslint/config"
+import { fixupConfigRules } from "@eslint/compat"
+import reactRefresh from "eslint-plugin-react-refresh"
+import globals from "globals"
+import tsParser from "@typescript-eslint/parser"
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import js from "@eslint/js"
+import { FlatCompat } from "@eslint/eslintrc"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default defineConfig([
+  globalIgnores(["**/dist", "**/.eslintrc.cjs"]),
+  {
+    extends: fixupConfigRules(
+      compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+      ),
+    ),
+
+    plugins: {
+      "react-refresh": reactRefresh,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+    },
+
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
+
+      // "@typescript-eslint/strict-boolean-expressions": "error",
+      // "@typescript-eslint/no-unsafe-argument": "error",
+      // "@typescript-eslint/no-unsafe-assignment": "error",
+      // "@typescript-eslint/no-unsafe-call": "error",
+      // "@typescript-eslint/no-unsafe-member-access": "error",
+      // "@typescript-eslint/no-unsafe-return": "error",
+      // "@typescript-eslint/restrict-template-expressions": "error",
+      // "@typescript-eslint/no-explicit-any": "error",
+      // "@typescript-eslint/no-unnecessary-type-assertion": "error",
+      // "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
+      // "@typescript-eslint/no-unnecessary-condition": "error",
+      // "@typescript-eslint/no-non-null-assertion": "error",
+      // "@typescript-eslint/explicit-module-boundary-types": "error",
+      // "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
+      // "@typescript-eslint/no-floating-promises": "error",
+      // "@typescript-eslint/require-await": "error",
+      // "@typescript-eslint/prefer-nullish-coalescing": "error",
+      // "@typescript-eslint/prefer-readonly": "error",
+      // "@typescript-eslint/prefer-reduce-type-parameter": "error",
+      //
+      // "@typescript-eslint/array-type": [
+      //   "error",
+      //   {
+      //     default: "array-simple",
+      //   },
+      // ],
+    },
+  },
+])
